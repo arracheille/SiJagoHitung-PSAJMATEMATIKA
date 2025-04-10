cc73973443e970836508fb0421d4db2f5cf9d4c7
arracheille <arachelrabbani@gmail.com>
2025-04-10 07:37:30 +0700

    first commit

---
+++ wordscramble/js/words.js
@@ -0,0 +1,98 @@
+let words = [
+  {
+    word: "penjumlahan",
+    hint: "Proses menambahkan bilangan",
+  },
+  {
+    word: "pengurangan",
+    hint: "Proses mengurangi satu bilangan dari bilangan lain",
+  },
+  {
+    word: "perkalian",
+    hint: "Proses menambah bilangan secara berulang",
+  },
+  {
+    word: "pembagian",
+    hint: "Proses membagi suatu bilangan menjadi bagian yang sama",
+  },
+  {
+    word: "pecahan",
+    hint: "Bilangan yang terdiri dari pembilang dan penyebut",
+  },
+  {
+    word: "desimal",
+    hint: "Bilangan yang memiliki tanda koma untuk memisahkan bagian utuh dan pecahan",
+  },
+  {
+    word: "persentase",
+    hint: "Bagian dari keseluruhan yang dinyatakan dalam bentuk persen",
+  },
+  {
+    word: "persamaan",
+    hint: "Pernyataan matematika yang menunjukkan dua ekspresi yang sama",
+  },
+  {
+    word: "variabel",
+    hint: "Simbol yang digunakan untuk mewakili nilai yang belum diketahui",
+  },
+  {
+    word: "geometri",
+    hint: "Cabang matematika yang mempelajari bentuk dan ruang",
+  },
+  {
+    word: "aljabar",
+    hint: "Cabang matematika yang mempelajari simbol dan aturan operasi",
+  },
+  {
+    word: "teorema",
+    hint: "Pernyataan yang dapat dibuktikan menggunakan logika dan matematika",
+  },
+  {
+    word: "integral",
+    hint: "Konsep dalam kalkulus yang digunakan untuk menghitung luas di bawah kurva",
+  },
+  {
+    word: "turunan",
+    hint: "Konsep dalam kalkulus yang menunjukkan laju perubahan",
+  },
+  {
+    word: "peluang",
+    hint: "Kemungkinan terjadinya suatu peristiwa",
+  },
+  {
+    word: "statistika",
+    hint: "Cabang matematika yang berhubungan dengan pengumpulan dan analisis data",
+  },
+  {
+    word: "matriks",
+    hint: "Susunan angka dalam baris dan kolom",
+  },
+  {
+    word: "koordinat",
+    hint: "Sekumpulan angka yang menunjukkan posisi suatu titik dalam ruang",
+  },
+  {
+    word: "fungsi",
+    hint: "Hubungan antara masukan dan keluaran di mana setiap masukan memiliki satu keluaran",
+  },
+  {
+    word: "jari-jari",
+    hint: "Jarak dari pusat lingkaran ke tepinya",
+  },
+  {
+    word: "pi",
+    hint: "Konstanta matematika yang kira-kira bernilai 3,14159",
+  },
+  {
+    word: "sudut",
+    hint: "Ukuran rotasi antara dua garis yang bertemu di satu titik",
+  },
+  {
+    word: "keliling",
+    hint: "Panjang total dari batas suatu bangun",
+  },
+  {
+    word: "volume",
+    hint: "Banyaknya ruang yang ditempati oleh suatu benda tiga dimensi",
+  },
+];
